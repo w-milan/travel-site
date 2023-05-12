2e523919c1f8d80c83fcb2dede2d0dc0fda69a76
milan <pperisic.milan@gmail.com>
2023-05-12 21:57:50 +0200

    Nav close button

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let navbar = document.querySelector('.header .navbar'); 
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () => {
+  navbar.classList.remove('active');
+}
+
+window.onscroll = () => {
+  navbar.classList.remove('active');
+}
