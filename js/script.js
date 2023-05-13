62488c80e763db6f2ef29ee150af19ebd325579b
milan <pperisic.milan@gmail.com>
2023-05-13 21:10:54 +0200

    Category section fixing, also script for navbar

--- js/script.js
+++ js/script.js
@@ -20,7 +20,23 @@ document.querySelector('#close-search').onclick = () => {
 
 window.onscroll = () => {
   navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
 }
+
+window.onload = () => {
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+}
+
 
 // Swipper JS 
 var swiper = new Swiper(".home-slider", {
