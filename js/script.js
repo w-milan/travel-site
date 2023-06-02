c303c816bfe00753d841bc30a9415d0085307821
milan <pperisic.milan@gmail.com>
2023-06-02 20:50:47 +0200

    Clients swiper

--- js/script.js
+++ js/script.js
@@ -129,3 +129,31 @@ var swiper = new Swiper(".blogs-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".clients-slider", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
+});
